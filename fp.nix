c86cafa23f34cc18b5254f9c1498920a953fc0d1
dvishal485 <26341736+dvishal485@users.noreply.github.com>
2024-11-07 23:29:36 +0530

    yay! it works
    Signed-off-by: dvishal485 <26341736+dvishal485@users.noreply.github.com>

--- fp.nix
+++ fp.nix
@@ -1,21 +1,21 @@
-{ stdenv, lib, fetchurl, dpkg, autoPatchelfHook, glib, gusb, pixman, libgudev, nss, tree, libfprint, cairo, pkg-config, coreutils }:
+{ stdenv, lib, fetchurl, rpm, cpio, glib, gusb, pixman, libgudev, nss, libfprint, cairo, pkg-config, libfprint-tod, autoPatchelfHook }:
 
 stdenv.mkDerivation rec {
-  pname = "libfprint-ft9366";
-  version = "1.94.4";
+  pname = "libfprint-2-tod1-focaltech";
+  libso = "libfprint-2.so.2.0.0";
+  version = "0.0.1";
 
   src = fetchurl {
-    url = "https://github.com/ftfpteams/RTS5811-FT9366-fingerprint-linux-driver-with-VID-2808-and-PID-a658/raw/b040ccd953c27e26c1285c456b4264e70b36bc3f/libfprint-2-2_1.94.4+tod1-0ubuntu1~22.04.2_amd64_rts5811.deb";
-    hash = "sha256-vH1SbJ+/Uro7IQKM/SNUo44ySZqMwj+SgmmzOmwAgpw=";
+    url = "https://github.com/ftfpteams/RTS5811-FT9366-fingerprint-linux-driver-with-VID-2808-and-PID-a658/raw/refs/heads/main/libfprint-2-2-1.94.4+tod1-FT9366_20240627.x86_64.rpm";
+    hash = "sha256-MRWHwBievAfTfQqjs1WGKBnht9cIDj9aYiT3YJ0/CUM=";
   };
 
   nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
+    rpm cpio
     pkg-config
+    autoPatchelfHook
+    libfprint-tod
   ];
-
-  # LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 
   buildInputs = [
     stdenv.cc.cc
@@ -29,18 +29,15 @@ stdenv.mkDerivation rec {
   ];
 
   unpackPhase = ''
-    dpkg-deb -x $src .
+    rpm2cpio $src | cpio -idmv
   '';
 
   installPhase = ''
-    cp -r . $out/
-
-    # move out to correct dir
-    mv $out/usr/lib/x86_64-linux-gnu/libfprint-2.so.2.0.0 $out/lib
-    mv $out/usr/lib/x86_64-linux-gnu/libfprint-2.so.2 $out/lib
+    install -Dm444 usr/lib64/${libso} -t $out/lib
 
     # create this symlink as it was there in libfprint
-    ln -s -T $out/lib/libfprint-2.so.2.0.0 $out/lib/libfprint-2.so
+    ln -s -T $out/lib/${libso} $out/lib/libfprint-2.so
+    ln -s -T $out/lib/${libso} $out/lib/libfprint-2.so.2
 
     # this .so was not marked executable by default
     chmod +x $out/lib/libfprint-2.so.2.0.0
