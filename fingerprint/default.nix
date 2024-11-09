aa785293897394577dca0784182c84a610ac3fdf
dvishal485 <26341736+dvishal485@users.noreply.github.com>
2024-11-09 17:36:54 +0530

    fix login
    Signed-off-by: dvishal485 <26341736+dvishal485@users.noreply.github.com>

--- fingerprint/default.nix
+++ fingerprint/default.nix
@@ -1,15 +1,14 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   nixpkgs.overlays = [ (self: super: { libfprint-focaltech = super.callPackage ./driver.nix { }; }) ];
   services.fprintd = {
     enable = true;
     package = pkgs.fprintd.override { libfprint = pkgs.libfprint-focaltech; };
   };
-  security.pam.services.sddm = lib.mkIf (config.services.fprintd.enable) {
-    text = ''
-      auth     [success=1 new_authtok_reqd=1 default=ignore]     pam_unix.so try_first_pass likeauth
-      nullok
-      auth     sufficient pam_fprintd.so
-    '';
-  };
+  security.pam.services.login.fprintAuth = false;
 }
