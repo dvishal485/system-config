017931a41a9a1a5ff96d13ead3e21d97336a5b13
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-11-12 13:32:19 +0530

    add startship config
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- home.nix
+++ home.nix
@@ -76,6 +76,14 @@
 
   programs.starship = {
     enable = true;
+    settings = {
+    command_timeout = 300;
+    scan_timeout = 10;
+      character = {
+        success_symbol = "[\\$](bold green)";
+        error_symbol = "[\\$](bold red)";
+      };
+    };
     enableBashIntegration = true;
   };
 
