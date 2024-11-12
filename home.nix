fe42074031616fa9df3ebfcaf6fe9bdadf496c64
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-11-12 15:29:52 +0530

    formatting
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- home.nix
+++ home.nix
@@ -77,8 +77,8 @@
   programs.starship = {
     enable = true;
     settings = {
-    command_timeout = 300;
-    scan_timeout = 10;
+      command_timeout = 300;
+      scan_timeout = 10;
       character = {
         success_symbol = "[\\$](bold green)";
         error_symbol = "[\\$](bold red)";
