f6645bb73494015928299a75d55dfddf3248c76c
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-11-27 17:17:39 +0530

    asus fan mode
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -27,6 +27,16 @@
     "asus_wmi.fnlock_default=N"
   ];
 
+  systemd.services.set-asus-fan-mode = {
+    description = "Set Asus Laptop Fan Mode to Performance Mode";
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      echo "0x00110019" > /sys/kernel/debug/asus-nb-wmi/dev_id &&
+      echo "2" > /sys/kernel/debug/asus-nb-wmi/ctrl_param &&
+      cat /sys/kernel/debug/asus-nb-wmi/devs || true
+    '';
+  };
+
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
