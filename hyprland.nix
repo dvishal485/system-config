2715c869193aeda3b51a0038106834dd6b4b2990
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-12-25 14:45:19 +0530

    init hyprland
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- hyprland.nix
+++ hyprland.nix
@@ -6,6 +6,31 @@
   ...
 }:
 {
+  environment.systemPackages = with pkgs; [
+    kitty
+    rofi
+    dunst
+    seahorse
+  ];
+
+  programs.hyprland.enable = true;
+  programs.hyprlock.enable = true;
+  programs.waybar = {
+    enable = true;
+  };
+  services.gnome.gnome-keyring.enable = true;
+
+  security.polkit.enable = true;
+  security.pam.services.hyprlock = { };
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   specialisation = {
     plasma-6.configuration = {
       system.nixos.tags = [ "plasma-6" ];
