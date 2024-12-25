0a25885925aa83706ef2f73ce04b61eac605aa2f
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-12-25 14:24:51 +0530

    refactor order
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- power_mgmt.nix
+++ power_mgmt.nix
@@ -28,8 +28,8 @@
 
       # AMD performance policy
       # [ performance, balance_performance, default, balance_power, power ]
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
       # AMD Turbo core
       CPU_BOOST_ON_AC = 1;
