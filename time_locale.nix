9a206099349ee39ace6536fe1a8f7530f8db9c36
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-12-26 13:54:34 +0530

    fix: locale
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- time_locale.nix
+++ time_locale.nix
@@ -14,15 +14,15 @@
   ];
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+    LC_ADDRESS = "en_IN.utf-8";
+    LC_IDENTIFICATION = "en_IN.utf-8";
+    LC_MEASUREMENT = "en_IN.utf-8";
+    LC_MONETARY = "en_IN.utf-8";
+    LC_NAME = "en_IN.utf-8";
+    LC_NUMERIC = "en_IN.utf-8";
+    LC_PAPER = "en_IN.utf-8";
+    LC_TELEPHONE = "en_IN.utf-8";
+    LC_TIME = "en_IN.utf-8";
   };
 
   # Configure keymap in X11
