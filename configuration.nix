153283e47fa62cabacf3e02033c47dda6fd959b9
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-01-15 15:16:08 +0530

    nerd fonts list
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,7 @@
   fonts.packages = with pkgs; [
     corefonts
     stix-two
+    # https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/data/fonts/nerdfonts/shas.nix
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
