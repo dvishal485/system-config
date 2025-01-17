2b9feb662c1813575bf60ebcbbffbdaed03941d6
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-01-17 19:34:11 +0530

    respect settings
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- .config/rofi/launchers/window.sh
+++ .config/rofi/launchers/window.sh
@@ -8,6 +8,7 @@ temp="$HOME/.cache/rofi-windows-preview"
 
 workspace_overview(){
     mkdir -p $temp
+    local animation=$(hyprctl getoption animations:enabled -j | jq .int)
     hyprctl keyword animations:enabled 0
     local active_win=$(hyprctl activewindow -j | jq ".address")
     local windows=$(hyprctl clients -j)
@@ -17,7 +18,7 @@ workspace_overview(){
 	# dunno why it doesn't work
 	# hyprctl dispatch focuswindow address:$active_win
 	echo $active_win | xargs -I {} hyprctl dispatch focuswindow address:{}
-	hyprctl keyword animations:enabled 1
+	hyprctl keyword animations:enabled $animation
 	local text=$(echo $windows | jq -r '.[] | "\(.title) - \(.address)\\0icon\\x1f'$temp'/\(.address).png\\n"')
 	selected=$(echo -en $text | rofi -dmenu -i -theme ${dir}/${theme}.rasi)
 	if [[ $selected != "" ]]; then
