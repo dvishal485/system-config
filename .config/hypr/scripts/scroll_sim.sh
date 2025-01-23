22708f4d2a8f7596904f1274e862e533d3b63bf5
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-01-23 18:41:20 +0530

    scroll wheel hack patch
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

---
+++ .config/hypr/scripts/scroll_sim.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+trap "exit 0" SIGINT SIGTERM
+
+scroll_ev(){
+    local base=$(hyprctl cursorpos -j | jq .y)
+    dotool < <(
+    while true; do
+        local curr=$(hyprctl cursorpos -j | jq .y)
+        local delta=$(( (curr - base) / 3 ))
+        base=$curr
+        printf "wheel $delta\n"
+    done
+    )
+}
+
+scroll_ev
