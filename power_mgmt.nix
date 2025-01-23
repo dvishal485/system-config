2e43cb1eec17e2e5c5c7436d940605fc0edc1184
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-01-23 15:55:04 +0530

    fmt
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- power_mgmt.nix
+++ power_mgmt.nix
@@ -59,7 +59,7 @@
       mkRule = as: lib.concatStringsSep ", " as;
       mkRules = rs: lib.concatStringsSep "\n" rs;
     in
-    mkRules ([
+    mkRules [
       (mkRule [
         ''ACTION=="add|change"''
         ''SUBSYSTEM=="block"''
@@ -67,5 +67,5 @@
         ''ATTR{queue/rotational}=="1"''
         ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k"''
       ])
-    ]);
+    ];
 }
