01c41f8063ae2e2fa156214935330c6d014d9e53
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-01-31 13:24:55 +0530

    add header
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- home-config-files.nix
+++ home-config-files.nix
@@ -1,3 +1,8 @@
+# Genrates a symlink for each file in the .config directory
+# Author: @dvishal485
+#
+# This file is auto-generated using the following command:
+# devbox run config
 { config, ... }:
 let
   nixHomeConfigPath = "/etc/nixos/.config";
