98bdb8fba5a2b2368196a0942a89ef98bd0e4cf3
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-02-01 00:54:13 +0530

    change temp
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- hyprland.nix
+++ hyprland.nix
@@ -58,8 +58,8 @@
       enable = true;
       latitude = 28.74;
       longitude = 77.11;
-      temperature.day = 6000;
-      temperature.night = 5600;
+      temperature.day = 5600;
+      temperature.night = 5000;
     };
   };
 
