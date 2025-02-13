4aec99513afef55ff4207123c4fb94dc84b70f19
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-02-13 21:09:08 +0530

    fan mode refactor
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- system.nix
+++ system.nix
@@ -12,7 +12,7 @@
   ];
 
   services.journald.extraConfig = "SystemMaxUse=256M";
-  systemd.services.set-asus-fan-mode = {
+  systemd.services.asus-fan-mode = {
     description = "Set Asus Laptop Fan Mode to Performance Mode";
     wantedBy = [ "multi-user.target" ];
     script = ''
@@ -20,6 +20,10 @@
       echo "2" > /sys/kernel/debug/asus-nb-wmi/ctrl_param &&
       cat /sys/kernel/debug/asus-nb-wmi/devs || true
     '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = false;
+    };
   };
 
   # https://nix.dev/manual/nix/2.18/command-ref/conf-file.html#conf-auto-optimise-store
