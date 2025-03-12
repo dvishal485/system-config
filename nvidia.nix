edf01fbc1549853b02bfeebe250f879b4577d55c
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-03-12 20:00:48 +0530

    upgraded kernel and nvidia drivers to latest
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- nvidia.nix
+++ nvidia.nix
@@ -71,7 +71,15 @@
 
     # changed in commit 4d711bc9394a61f804fb6e4aa0e04fadd642fa73
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    # package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.124.04";
+      sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
+      sha256_aarch64 = "sha256-HN58N00SNEwMQKSKuOMAXVW6J2VI/2YyDulQNJHuVeM=";
+      openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
+      settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
+      persistencedSha256 = "sha256-SHSdnGyAiRH6e0gYMYKvlpRSH5KYlJSA1AJXPm7MDRk=";
+    };
   };
 
   specialisation = {
