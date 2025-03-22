894979a6ec48378f96e1d08902f6ac03b9691907
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-03-22 12:51:46 +0530

    perf mode now asks instead of asserts
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- .config/hypr/scripts/perf_mode.sh
+++ .config/hypr/scripts/perf_mode.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env sh
 
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+
 if [ "$HYPRGAMEMODE" = 1 ] ; then
-    notify-send "Game Mode" "Game Mode enabled" -i settings -e
-    hyprctl keyword monitor eDP-1,highres,highrr,1.0
-    hyprctl --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:shadow:enabled 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+    action=$(notify-send "Game Mode" "Game mode is not running, do you want to enable it?" -i settings -a "hyprctl" -u critical -c "Disable,Enable" -A enable=Enable -A dismiss=Dismiss)
+    if [ "$action" = "enable" ] ; then
+        hyprctl keyword monitor eDP-1,highres,highrr,1.0
+        hyprctl --batch "\
+            keyword animations:enabled 0;\
+            keyword decoration:shadow:enabled 0;\
+            keyword decoration:blur:enabled 0;\
+            keyword general:gaps_in 0;\
+            keyword general:gaps_out 0;\
+            keyword general:border_size 1;\
+            keyword decoration:rounding 0"
+    fi
     exit
-else
-    notify-send "Game Mode" "Game Mode disabled" -i settings -e
 fi
-hyprctl reload
+
+action=$(notify-send "Game Mode" "Game mode is active, do you want to disable it?" -i settings -a "hyprctl" -u critical -c "Disable,Enable" -A disable=Disable -A dismiss=Dismiss)
+if [ "$action" = "disable" ] ; then
+    hyprctl reload
+fi
