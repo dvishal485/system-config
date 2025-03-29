4d3edfa7995fe750dba009943d6a12ebf60d1072
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-03-29 22:35:18 +0530

    useless ssh-auth-sock export which may help in future
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- system.nix
+++ system.nix
@@ -47,6 +47,9 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = false;
+  services.xserver.displayManager.sessionCommands = ''
+    export SSH_AUTH_SOCK
+  '';
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
