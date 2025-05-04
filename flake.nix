80f4e287e242908230f42c2fc9aa31cd73f83b4c
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-05-04 15:27:51 +0530

    remove nix-gaming
    flake input of nix gaming removed

--- flake.nix
+++ flake.nix
@@ -6,10 +6,9 @@
     btrfs-simple-snapshot.url = "github:dvishal485/btrfs-simple-snapshot/v0.1.6";
 
     nh.url = "github:nix-community/nh/v4.0.3";
-    nh.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-gaming.url = "github:fufexan/nix-gaming";
+    # nix-gaming.url = "github:fufexan/nix-gaming";
 
     hyprland.url = "git+https://github.com/hyprwm/hyprland?ref=refs/tags/v0.46.2&submodules=1";
 
