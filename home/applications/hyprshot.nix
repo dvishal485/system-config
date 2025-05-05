42e0fdf1f247835e5887789b06424d0370bb1045
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-05-06 00:32:13 +0530

    make hyprshot notification native feature

--- home/applications/hyprshot.nix
+++ home/applications/hyprshot.nix
@@ -1,26 +1,34 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
-  hyprshot-notification = pkgs.writeScriptBin "hyprshot-notification" ''
+  hyprshot-package = pkgs.hyprshot;
+  hyprshot-dir = "${config.home.homeDirectory}/Pictures/Screenshots";
+  custom-hyprshot = pkgs.writeScriptBin "hyprshot" ''
     #!/usr/bin/env sh
-    action=$(${pkgs.libnotify}/bin/notify-send "Screenshot saved" \
-      "Screenshot saved to path $@" \
-      -t 5000 -i "$@" -a Hyprshot \
-      -A default=show -A dir='Show directory')
 
-    if [[ "$action" == "default" ]]; then
-      ${pkgs.xdg-utils}/bin/xdg-open "$@"
-    elif [[ "$action" == "dir" ]]; then
-      ${pkgs.xdg-utils}/bin/xdg-open "$(${pkgs.coreutils}/bin/dirname "$@")"
-    fi
+    hyprshot_notification() {
+      local screenshot_path="$1"
+      action=$(${pkgs.libnotify}/bin/notify-send "Screenshot saved" \
+        "Screenshot saved to path $screenshot_path" \
+        -t 5000 -i "$screenshot_path" -a Hyprshot \
+        -A default=show -A dir='Show directory' -A delete='Delete')
+
+      if [[ "$action" == "default" ]]; then
+        ${pkgs.xdg-utils}/bin/xdg-open "$screenshot_path"
+      elif [[ "$action" == "dir" ]]; then
+        local dir=$(${pkgs.coreutils}/bin/dirname "$screenshot_path")
+        ${pkgs.xdg-utils}/bin/xdg-open "$dir"
+      elif [[ "$action" == "delete" ]]; then
+        ${pkgs.coreutils}/bin/rm "$screenshot_path"
+      fi
+    }
+
+    export -f hyprshot_notification
+
+    HYPRSHOT_DIR=${hyprshot-dir} ${hyprshot-package}/bin/hyprshot "$@" -- "hyprshot_notification"
   '';
 in
 {
   home.packages = [
-    hyprshot-notification
-    pkgs.hyprshot
+    custom-hyprshot
   ];
-
-  home.sessionVariables = {
-    HYPRSHOT_DIR = "$HOME/Pictures/Screenshots";
-  };
 }
