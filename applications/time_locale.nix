0db147916a91c89d4fb93236978fcc8d0397b8d4
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-05-20 13:51:34 +0530

    fix: time locale

--- applications/time_locale.nix
+++ applications/time_locale.nix
@@ -9,21 +9,20 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_IN";
-  i18n.supportedLocales = [
+  i18n.extraLocales = [
     "en_US.UTF-8/UTF-8"
-    "en_IN/UTF-8"
   ];
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN.utf-8";
-    LC_IDENTIFICATION = "en_IN.utf-8";
-    LC_MEASUREMENT = "en_IN.utf-8";
-    LC_MONETARY = "en_IN.utf-8";
-    LC_NAME = "en_IN.utf-8";
-    LC_NUMERIC = "en_IN.utf-8";
-    LC_PAPER = "en_IN.utf-8";
-    LC_TELEPHONE = "en_IN.utf-8";
-    LC_TIME = "en_IN.utf-8";
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
   };
 
   # Configure keymap in X11
