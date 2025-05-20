d4137155b58302e4eaa68bf2912ac7348819ceff
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-05-20 14:03:49 +0530

    use prod nvidia drivers

--- hosts/rio/specialisations/gaming.nix
+++ hosts/rio/specialisations/gaming.nix
@@ -16,14 +16,7 @@
 
         # xanmod kernel
         boot.kernelPackages = lib.mkForce kernel;
-        hardware.nvidia.package = kernel.nvidiaPackages.mkDriver {
-          version = "570.133.07";
-          sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
-          sha256_aarch64 = "sha256-yTovUno/1TkakemRlNpNB91U+V04ACTMwPEhDok7jI0=";
-          openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
-          settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
-          persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
-        };
+        hardware.nvidia.package = kernel.nvidiaPackages.production;
 
         imports = [
           ../modules/gaming.nix
