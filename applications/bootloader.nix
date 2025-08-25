1b797dec2e10d47c39b641e6abecf0206308aa2c
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-08-25 20:01:01 +0530

    show boot menu

--- applications/bootloader.nix
+++ applications/bootloader.nix
@@ -6,6 +6,6 @@ _: {
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
-    timeout = 0;
+    timeout = 10;
   };
 }
